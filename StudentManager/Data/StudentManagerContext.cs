6fbd71817d05e92ebdb603f1193a28990cf688ac
Kalinga-Shamel-Silva <shamelsilvash@gmail.com>
2024-10-24 20:07:13 +0530

    Initial adding files
    StudentManager
    Database file
    SQL Files

---
+++ StudentManager/Data/StudentManagerContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StudentManager.Models;
+
+namespace StudentManager.Data
+{
+    public class StudentManagerContext : DbContext
+    {
+        public StudentManagerContext (DbContextOptions<StudentManagerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<StudentManager.Models.Course> Course { get; set; } = default!;
+        public DbSet<StudentManager.Models.Student> Student { get; set; } = default!;
+    }
+}
---
+++ StudentManager/Pages/Students/Create.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using StudentManager.Data;
+using StudentManager.Models;
+
+namespace StudentManager.Pages.Students
+{
+    public class CreateModel : PageModel
+    {
+        private readonly StudentManager.Data.StudentManagerContext _context;
+
+        public CreateModel(StudentManagerContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Student Student { get; set; } = default!;
+
+        public IList<Course> Course { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Course = await _context.Course.ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Student.Add(Student);
+            await _context.SaveChangesAsync();
+            return RedirectToPage("./Index");
+        }
+    }
+
+
+}
